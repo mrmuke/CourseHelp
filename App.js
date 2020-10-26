bcb00b72d1d58cbe0dc2c74adade739beb195b56
Aaron Zhang <yunjian2000@yahoo.com>
2020-10-25 20:04:46 -0500

    Google SignIn first commit

--- App.js
+++ App.js
@@ -1,20 +1,40 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { createSwitchNavigator, createAppContainer } from 'react-navigation'
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+import LoginScreen from './screens/LoginScreen'
+import LoadingScreen from './screens/LoadingScreen'
+import DashboardScreen from './screens/DashboardScreen'
+//import { render } from 'react-dom';
+
+import * as firebase from 'firebase';
+import { firebaseConfig } from './config';
+
+if (!firebase.apps.length) {
+  firebase.initializeApp(firebaseConfig);
 }
+
+
+export default class App extends React.Component {
+  render() {
+    return <AppNavigator />;
+  }
+}
+
+const AppSwitchNavigator = createSwitchNavigator({
+  LoadingScreen: LoadingScreen,
+  LoginScreen: LoginScreen,
+  DashboardScreen: DashboardScreen
+});
+
+const AppNavigator = createAppContainer
+  (AppSwitchNavigator);
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#0000',
     alignItems: 'center',
     justifyContent: 'center',
   },
---
+++ screens/DashboardScreen.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+//import { render } from 'react-dom';
+import { View, Text, StyleSheet, Button } from 'react-native';
+import firebase from 'firebase'
+class DashboardScreen extends Component {
+    render() {
+        return (
+            <View style={styles.container}>
+                <Text>DashboardScreen</Text>
+                <Button title="Sign out" onPress={() => firebase.auth().signOut()} />
+            </View>
+        );
+    }
+
+}
+
+export default DashboardScreen;
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center'
+    }
+})
